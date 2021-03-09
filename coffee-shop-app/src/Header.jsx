56481554b99d181299c99f562a552560a9d2e32d
HaNguyen110 <hanguyen091130@gmail.com>
2021-03-09 16:52:05 +0700

    apply bootstrap

--- coffee-shop-app/src/Header.jsx
+++ coffee-shop-app/src/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { Button, Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import './App.css';
 
 function Header() {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <NavbarBrand href="home">Coffee Shop</NavbarBrand>
+                <Navbar.Brand href="#home">Coffee Shop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <NavDropdown title="Another" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#Drop1">products</NavDropdown.Item>
+                            <NavDropdown.Item href="#Drop2"> price</NavDropdown.Item>
+                            <NavDropdown.Item href="#Drop3">contact</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Sign In</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Logn In  
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-            
+
             <div className="text-light text-right Header--banner">
                 <h1 className="display-4 text-center font-weight-bold Header--text">
                     Coffee Shop
